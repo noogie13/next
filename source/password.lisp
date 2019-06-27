066b5660579c882db20f34e4756df43487964587
Solomon Bloch <solomonbloch@gmail.com>
2019-06-27 08:58:53 -0400

    Deleting save-password stuff for clip-only-branch

--- source/password.lisp
+++ source/password.lisp
@@ -35,9 +35,6 @@
 (defmethod clip-password ((password-interface password-store-interface) password-name)
   (uiop:run-program `("pass" "--clip" ,password-name)))
 
-(defmethod save-password ((password-interface password-store-interface) password-name password)
-  (uiop:run-program (str:concat "echo \"" password "\" | pass insert --echo " password-name)))
-
 (defun copy-password-completion-fn (password-instance)
   (let ((password-list (list-passwords password-instance)))
     (lambda (input)
@@ -49,35 +46,5 @@
                                (minibuffer *interface*)
                                :completion-function (copy-password-completion-fn (password *interface*))))
     (clip-password (password *interface*) password-name)))
-
-(defun generate-input-html-original (input-buffer cursor-index)
-  (cond ((equal "" input-buffer) (cl-markup:markup (:span :id "cursor" (cl-markup:raw "&nbsp;"))))
-        ((eql cursor-index (length input-buffer)) (cl-markup:markup (:span input-buffer)
-                                                                    (:span :id "cursor" (cl-markup:raw "&nbsp;"))))
-        (t (cl-markup:markup (:span (subseq input-buffer 0 cursor-index))
-                             (:span :id "cursor" (subseq input-buffer cursor-index (+ 1 cursor-index)))
-                             (:span (subseq input-buffer (+ 1  cursor-index)))))))
-
-(defun generate-input-html-new (input-buffer cursor-index)
-  (let ((input-buffer-password (make-string (length input-buffer) :initial-element #\*)))
-    (cond ((equal "" input-buffer-password) (cl-markup:markup (:span :id "cursor" (cl-markup:raw "&nbsp;"))))
-          ((eql cursor-index (length input-buffer-password)) (cl-markup:markup (:span input-buffer-password)
-                                                                               (:span :id "cursor" (cl-markup:raw "&nbsp;"))))
-          (t (cl-markup:markup (:span (subseq input-buffer-password 0 cursor-index))
-                               (:span :id "cursor" (subseq input-buffer-password cursor-index (+ 1 cursor-index)))
-                               (:span (subseq input-buffer-password (+ 1  cursor-index))))))))
-
-(define-command save-new-password ()
-  "Saves password to password interface."
-  (with-result (password-name (read-from-minibuffer
-                               (minibuffer *interface*)
-                               :input-prompt "Name for new password:"))
-    (setf (symbol-function 'generate-input-html) (symbol-function 'generate-input-html-new))
-    (with-result (password (read-from-minibuffer
-                            (minibuffer *interface*)
-                            :input-prompt "New password:"))
-      (save-password (password *interface*) password-name password)))
-  (setf (symbol-function 'generate-input-html) (symbol-function 'generate-input-html-original)))
-
 
 ;;; to test this, load this file from init, and (setf (password *interface*) (make-instance 'password-store-interface))
