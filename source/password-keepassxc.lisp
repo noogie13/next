e089cd3ce798b23484a02074e628a9a732897ba7
Solomon Bloch <solomonbloch@gmail.com>
2019-08-22 00:59:39 -0400

    Fix LIST-PASSWORDS, add SAVE-PASSWORD, PASSWORD-CORRECT-P.

--- source/password-keepassxc.lisp
+++ source/password-keepassxc.lisp
@@ -1,27 +1,37 @@
 (in-package :next)
 
-(defmacro with-password (password-interface &body body)
-  `(if (eq 'keepassxc-interface (type-of ,password-interface))
-       (if (null (master-password ,password-interface))
-           (progn
-             (with-result (password-set (read-from-minibuffer
-                                         (minibuffer *interface*)
-                                         :invisible-input-p t
-                                         :input-prompt "Password:"))
-               (setf (master-password ,password-interface) password-set)
-               ,@body))
-           ,@body)
-       ,@body))
-
 (defmethod list-passwords ((password-interface keepassxc-interface))
-  (let* ((st (make-string-input-stream (password password-interface)))
-         (output (uiop:run-program `("keepassxc-cli" "ls" ,(password-file password-interface))
+  (let* ((st (make-string-input-stream (master-password password-interface)))
+         (output (uiop:run-program `("keepassxc-cli"
+                                     "ls" ,(password-file password-interface))
                                    :input st :output '(:string :stripped t))))
     (remove "Recycle Bin/" (rest (cl-ppcre:split "\\n" output)) :test #'equal)))
 
 (defmethod clip-password ((password-interface keepassxc-interface) password-name)
-  (let* ((st (make-string-input-stream (password password-interface)))
-         (output (uiop:run-program `("keepassxc-cli" "show" ,(password-file password-interface) ,password-name)
+  (let* ((st (make-string-input-stream (master-password password-interface)))
+         (output (uiop:run-program `("keepassxc-cli"
+                                     "show" ,(password-file password-interface)
+                                     ,password-name)
                                    :input st :output '(:string :stripped t))))
     (clip-password-string
      (cl-ppcre:regex-replace "[\\S\\s]*Password: \(.*\)[\\S\\s]*" output "\\1"))))
+
+(defmethod save-password ((password-interface keepassxc-interface) password-name password)
+  (with-input-from-string (st (make-string-input-stream
+                               (format nil "~a~C~a"
+                                       (master-password password-interface)
+                                       #\newline password)))
+    (uiop:run-program `("keepassxc-cli"
+                        "add" "-p" ,(password-file password-interface) ,password-name)
+                      :input st)))
+
+(defmethod password-correct-p ((password-interface keepassxc-interface))
+  (when (master-password password-interface)
+    (handler-case
+        (let* ((st (make-string-input-stream (master-password password-interface)))
+               (output (uiop:run-program `("keepassxc-cli"
+                                           "ls" ,(password-file password-interface))
+                                         :input st :output '(:string :stripped t))))
+          (remove "Recycle Bin/" (rest (cl-ppcre:split "\\n" output)) :test #'equal))
+      (uiop/run-program:subprocess-error ()
+        nil))))
